e92ee97d434d80f0da7968782d8df11ab3781c40
Michael Schams <typo3@2019.schams.net>
2019-03-20 02:53:23 +1100

    [TASK] Style FE user login form

--- Resources/Public/JavaScript/website.js
+++ Resources/Public/JavaScript/website.js
@@ -11,3 +11,18 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
+
+ $(document).ready(function() {
+     $('.tx-felogin-pi1 form fieldset div').each(function(id) {
+         $(this).addClass('form-group');
+     })
+     $('.tx-felogin-pi1 form fieldset div input').each(function(id) {
+         var type = $(this).attr('type');
+         if (type == 'text' || type == 'password') {
+             $(this).addClass('form-control');
+         }
+         if (type == 'submit') {
+             $(this).addClass('btn btn-primary');
+         }
+     });
+ });
